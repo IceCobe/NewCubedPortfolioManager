332fc583e45b4f092fc388a2d530290094f0b773
IceCobe <jaalaba@gmail.com>
2022-07-13 23:17:06 -0400

    First html page rendered
    Incomplete: Bootstrap, components added, figure out how to apply bootstrap

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the portfolio index.")
+    return render(request, "portfolio.html")
